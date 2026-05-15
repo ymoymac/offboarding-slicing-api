0991c207583812b98c594093673698f4e4610c60
Yael Jaffar Moya Macias <ymoya@izzi.mx>
2026-05-14 18:35:05 -0600

    Set. Ready. Go

---
+++ src/main/java/mx/izzi/offboarding/OffboardingApplication.java
@@ -0,0 +1,13 @@
+package mx.izzi.offboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/izzi/offboarding/OffboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.izzi.offboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
